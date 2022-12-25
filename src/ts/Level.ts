b112f5244f6adaf1d0cb48f6b0da995e40e5cfa2
Ivan <ivan.kolipov@gmail.com>
2022-12-25 16:31:57 +0300

    Indentation normalization

--- src/ts/Level.ts
+++ src/ts/Level.ts
@@ -1,138 +1,141 @@
-import { finishModal, modalNode } from "./index";
+import { finishModal, modalNode } from './index';
 
-import { ICoords } from "./types";
+import { ICoords } from './types';
 
-const gameNode = document.querySelector("#game")!;
+const gameNode = document.querySelector('#game')!;
 
 export default class Level {
-  private readonly gameCells: Array<NodeListOf<HTMLDivElement>> = [];
-  private readonly currentCoords: ICoords;
-  private isFinished = false;
+	private readonly gameCells: Array<NodeListOf<HTMLDivElement>> = [];
+	private readonly currentCoords: ICoords;
+	private isFinished = false;
 
-  constructor(
-    private readonly rows: number,
-    private readonly cells: number,
-    private readonly finishCoords: ICoords,
-    private readonly spawnCoords: ICoords,
-    private readonly onSubmit: (instance: Level) => void,
-    public levelNumber: number
-  ) {
-    this.currentCoords = spawnCoords;
-    void this.createField();
-    this.init();
-  }
+	constructor(
+		private readonly rows: number,
+		private readonly cells: number,
+		private readonly finishCoords: ICoords,
+		private readonly spawnCoords: ICoords,
+		private readonly onSubmit: (instance: Level) => void,
+		public levelNumber: number
+	) {
+		this.currentCoords = spawnCoords;
+		void this.createField();
+		this.init();
+	}
 
-  createField = async (): Promise<void> => {
-    for (let row = 0; row < this.rows; row++) {
-      // row
-      const rowEl = document.createElement("div");
-      rowEl.className = "game-row";
+	createField = async (): Promise<void> => {
+		for (let row = 0; row < this.rows; row++) {
+			// row
+			const rowEl = document.createElement('div');
+			rowEl.className = 'game-row';
 
-      for (let cell = 0; cell < this.cells; cell++) {
-        // cell
-        const cellEl = document.createElement("div");
-        cellEl.classList.add("game-cell");
-        if (this.finishCoords.y === row && this.finishCoords.x === cell) {
-          cellEl.classList.add("finish");
-        }
-        if (this.spawnCoords.y === row && this.spawnCoords.x === cell) {
-          cellEl.classList.add("player");
-        }
-        // rowEl.insertAdjacentElement('afterend', cellEl);
-        rowEl.innerHTML += cellEl.outerHTML;
-      }
-      gameNode.insertAdjacentElement("beforeend", rowEl);
-    }
-  };
+			for (let cell = 0; cell < this.cells; cell++) {
+				// cell
+				const cellEl = document.createElement('div');
+				cellEl.classList.add('game-cell');
+				if (
+					this.finishCoords.y === row &&
+					this.finishCoords.x === cell
+				) {
+					cellEl.classList.add('finish');
+				}
+				if (this.spawnCoords.y === row && this.spawnCoords.x === cell) {
+					cellEl.classList.add('player');
+				}
+				// rowEl.insertAdjacentElement('afterend', cellEl);
+				rowEl.innerHTML += cellEl.outerHTML;
+			}
+			gameNode.insertAdjacentElement('beforeend', rowEl);
+		}
+	};
 
-  init = (): void => {
-    document.querySelectorAll(".game-row").forEach((row) => {
-      this.gameCells.push(row.querySelectorAll(".game-cell"));
-    });
+	init = (): void => {
+		document.querySelectorAll('.game-row').forEach((row) => {
+			this.gameCells.push(row.querySelectorAll('.game-cell'));
+		});
 
-    document.querySelector("#current-level")!.textContent =
-      this.levelNumber.toString();
+		document.querySelector('#current-level')!.textContent =
+			this.levelNumber.toString();
 
-    document.addEventListener(`keydown`, this.onKeyDown);
-  };
+		document.addEventListener(`keydown`, this.onKeyDown);
+	};
 
-  onKeyDown = (e: KeyboardEvent): void => {
-    const MAX_ROW_INDEX = this.rows - 1;
-    const MAX_CELL_INDEX = this.cells - 1;
+	onKeyDown = (e: KeyboardEvent): void => {
+		const MAX_ROW_INDEX = this.rows - 1;
+		const MAX_CELL_INDEX = this.cells - 1;
 
-    if (this.isFinished) return;
+		if (this.isFinished) return;
 
-    switch (e.key) {
-      case "ArrowUp":
-        if (this.currentCoords.y - 1 >= 0) {
-          this.currentCoords.y -= 1;
-        }
-        this.render();
-        break;
-      case "ArrowDown":
-        if (this.currentCoords.y + 1 <= MAX_ROW_INDEX) {
-          this.currentCoords.y += 1;
-        }
-        this.render();
-        break;
-      case "ArrowLeft":
-        if (this.currentCoords.x - 1 >= 0) {
-          this.currentCoords.x -= 1;
-        }
-        this.render();
-        break;
-      case "ArrowRight":
-        if (this.currentCoords.x + 1 <= MAX_CELL_INDEX) {
-          this.currentCoords.x += 1;
-        }
-        this.render();
-        break;
-    }
-  };
+		switch (e.key) {
+			case 'ArrowUp':
+				if (this.currentCoords.y - 1 >= 0) {
+					this.currentCoords.y -= 1;
+				}
+				this.render();
+				break;
+			case 'ArrowDown':
+				if (this.currentCoords.y + 1 <= MAX_ROW_INDEX) {
+					this.currentCoords.y += 1;
+				}
+				this.render();
+				break;
+			case 'ArrowLeft':
+				if (this.currentCoords.x - 1 >= 0) {
+					this.currentCoords.x -= 1;
+				}
+				this.render();
+				break;
+			case 'ArrowRight':
+				if (this.currentCoords.x + 1 <= MAX_CELL_INDEX) {
+					this.currentCoords.x += 1;
+				}
+				this.render();
+				break;
+		}
+	};
 
-  render = (): void => {
-    this.clearField();
-    this.gameCells.forEach((row, rowIndex) => {
-      row.forEach((cell, cellIndex) => {
-        if (
-          this.currentCoords.y === rowIndex &&
-          this.currentCoords.x === cellIndex
-        ) {
-          cell.classList.add("player");
-        }
-      });
-    });
-    if (
-      this.currentCoords.y === this.finishCoords.y &&
-      this.currentCoords.x === this.finishCoords.x
-    ) {
-      if (!this.isFinished) {
-        this.finish();
-      }
-    }
-  };
+	render = (): void => {
+		this.clearField();
+		this.gameCells.forEach((row, rowIndex) => {
+			row.forEach((cell, cellIndex) => {
+				if (
+					this.currentCoords.y === rowIndex &&
+					this.currentCoords.x === cellIndex
+				) {
+					cell.classList.add('player');
+				}
+			});
+		});
+		if (
+			this.currentCoords.y === this.finishCoords.y &&
+			this.currentCoords.x === this.finishCoords.x
+		) {
+			if (!this.isFinished) {
+				this.finish();
+			}
+		}
+	};
 
-  clearField = (): void => {
-    this.gameCells.forEach((row) => {
-      row.forEach((cell) => {
-        cell.classList.remove("player");
-      });
-    });
-  };
+	clearField = (): void => {
+		this.gameCells.forEach((row) => {
+			row.forEach((cell) => {
+				cell.classList.remove('player');
+			});
+		});
+	};
 
-  finish = (): void => {
-    modalNode.querySelector("#level")!.textContent =
-      this.levelNumber.toString();
-    modalNode.querySelector("#submit")!.addEventListener(`click`, () => {
-      this.onSubmit(this);
-      finishModal.hide();
-    });
-    this.isFinished = true;
-    finishModal.show();
-  };
+	finish = (): void => {
+		modalNode.querySelector('#level')!.textContent =
+			this.levelNumber.toString();
+		modalNode.querySelector('#submit')!.addEventListener(`click`, () => {
+			this.onSubmit(this);
+			finishModal.hide();
+		});
+		this.isFinished = true;
+		finishModal.show();
+	};
 
-  destroyLevel = (): void => {
-    gameNode.innerHTML = ``;
-    document.removeEventListener(`keydown`, this.onKeyDown);
-  };
+	destroyLevel = (): void => {
+		gameNode.innerHTML = ``;
+		document.removeEventListener(`keydown`, this.onKeyDown);
+	};
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,11 @@
-import { Modal } from "bootstrap";
+import { Modal } from 'bootstrap';
 
-import "../css/index.css";
-import Level from "./Level";
-import { levels } from "./levels";
+import '../css/index.css';
+import Level from './Level';
+import { levels } from './levels';
 
-export const modalNode = document.querySelector("#finish-modal")!;
-export const finishModal = new Modal("#finish-modal");
+export const modalNode = document.querySelector('#finish-modal')!;
+export const finishModal = new Modal('#finish-modal');
 
 let levelIndex = 0;
 
@@ -14,30 +14,30 @@ let prevLevel: Level;
 renderCurrentLevel();
 
 function renderCurrentLevel(): void {
-  prevLevel?.destroyLevel();
-  const currentLevel = levels[levelIndex];
-  if (currentLevel !== undefined) {
-    const rows = currentLevel.size.rows;
-    const cells = currentLevel.size.cells;
-    const spawnCoords = currentLevel.spawnCoords;
-    const finishCoords = currentLevel.finishCoords;
+	prevLevel?.destroyLevel();
+	const currentLevel = levels[levelIndex];
+	if (currentLevel !== undefined) {
+		const rows = currentLevel.size.rows;
+		const cells = currentLevel.size.cells;
+		const spawnCoords = currentLevel.spawnCoords;
+		const finishCoords = currentLevel.finishCoords;
 
-    prevLevel = new Level(
-      rows,
-      cells,
-      finishCoords,
-      spawnCoords,
-      nextLevel,
-      currentLevel.level
-    );
-  } else {
-    new Modal("#game-end-modal").show();
-  }
+		prevLevel = new Level(
+			rows,
+			cells,
+			finishCoords,
+			spawnCoords,
+			nextLevel,
+			currentLevel.level
+		);
+	} else {
+		new Modal('#game-end-modal').show();
+	}
 }
 
 function nextLevel(instance: Level): void {
-  if (instance.levelNumber === levelIndex + 1) {
-    levelIndex++;
-    renderCurrentLevel();
-  }
+	if (instance.levelNumber === levelIndex + 1) {
+		levelIndex++;
+		renderCurrentLevel();
+	}
 }
