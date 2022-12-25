01a53f75591d1bf7622166039c989c0dab66a801
Ivan <ivan.kolipov@gmail.com>
2022-12-25 17:23:00 +0300

    Add selectors enums

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -12,3 +12,23 @@ export interface ILevel {
 	spawnCoords: ICoords;
 	finishCoords: ICoords;
 }
+
+export const enum Selectors {
+	CURRENT_LEVEL_SPAN = '#current-level'
+}
+
+export const enum FinishModalSelectors {
+	FINISH_MODAL = '#finish-modal',
+	LEVEL_SPAN = '#level',
+	RETRY_BTN = '#retry-btn',
+	SUBMIT_BTN = '#submit-btn'
+}
+
+export const enum ControlsSelectors {
+	CONTROLS_HINT = '#controls-hint',
+	MOBILE_CONTROLS = '#controls',
+	UP_BUTTON = '#up-btn',
+	DOWN_BUTTON = '#down-btn',
+	LEFT_BUTTON = '#left-btn',
+	RIGHT_BUTTON = '#right-btn'
+}
