547fbe7823596e8b42ed65d0f8d5e9b5ede1365f
Ivan Kolipov <ivan.kolipov@gmail.com>
2022-12-26 23:13:05 +0300

    Add levels (5)

--- src/ts/levels.ts
+++ src/ts/levels.ts
@@ -2,12 +2,12 @@ import { CellRole, ILevel } from './types';
 
 export const levels: ILevel[] = [
 	{
-		levelName: '1',
+		levelName: '1 easy',
 		field: [
 			[
 				{ role: CellRole.EMPTY },
-				{ role: CellRole.PLAYER },
-				{ role: CellRole.WALL }
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.PLAYER }
 			],
 			[
 				{ role: CellRole.EMPTY },
@@ -22,30 +22,160 @@ export const levels: ILevel[] = [
 		]
 	},
 	{
-		levelName: '2',
+		levelName: '2 cave',
 		field: [
+			[
+				{ role: CellRole.PLAYER },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
+			],
+			[
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
+			],
+			[
+				{ role: CellRole.WALL },
+				{ role: CellRole.FINISH },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
+			], [
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL }
+			]
+		]
+	},
+	{
+		levelName: '3',
+		field: [
+			[
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL }
+			],
 			[
 				{ role: CellRole.PLAYER },
 				{ role: CellRole.WALL },
 				{ role: CellRole.WALL },
-				{ role: CellRole.WALL }
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
 			],
 			[
-				{ role: CellRole.EMPTY },
 				{ role: CellRole.WALL },
 				{ role: CellRole.WALL },
-				{ role: CellRole.WALL }
-			],
-			[
-				{ role: CellRole.EMPTY },
 				{ role: CellRole.FINISH },
-				{ role: CellRole.WALL }
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
+			], [
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
 			], [
 				{ role: CellRole.EMPTY },
 				{ role: CellRole.EMPTY },
-				{ role: CellRole.WALL },
-				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
+			]
+		]
+	},
+	{
+		levelName: '4 troll',
+		field: [
+			[
+				{ role: CellRole.FINISH },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
 				{ role: CellRole.WALL }
+			],
+			[
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
+			],
+			[
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.PLAYER },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
+			], [
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
+			], [
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
+			]
+		]
+	},
+	{
+		levelName: '5 think',
+		field: [
+			[
+				{ role: CellRole.PLAYER },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.FINISH }
+			],
+			[
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
+			],
+			[
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
+			],
+			[
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.EMPTY }
+			],
+			[
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY },
+				{ role: CellRole.WALL },
+				{ role: CellRole.EMPTY }
 			]
 		]
 	}
